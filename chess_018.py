3cdf292fc8ea234d3b32ac33f3a65a34e79c4172
BlueTot <bluetotkc@gmail.com>
2023-07-08 20:27:10 +0100

    latest version

--- chess_018.py
+++ chess_018.py
@@ -1,17 +1,8 @@
 import chess.polyglot
 import chess.svg
-import pygame
-import cairosvg
-import io
 import math
 import time
 import copy
-
-
-class Modes:
-    PLAYER_VS_COMPUTER = 0
-    COMPUTER_ONLY = 1
-    PLAYER_ONLY = 2
 
 
 def reverse(lst):
@@ -111,11 +102,11 @@ def evaluate(board):
                     case chess.KING:
                         Eval += mult * (900 + (KING if piece.color else R_KING)[row][col])
                     case chess.QUEEN:
-                        Eval += mult * (90 + (QUEEN if piece.color else R_QUEEN)[row][col])
+                        Eval += mult * (88 + (QUEEN if piece.color else R_QUEEN)[row][col])
                     case chess.ROOK:
-                        Eval += mult * (50 + (ROOK if piece.color else R_ROOK)[row][col])
+                        Eval += mult * (51 + (ROOK if piece.color else R_ROOK)[row][col])
                     case chess.BISHOP:
-                        Eval += mult * (30 + (BISHOP if piece.color else R_BISHOP)[row][col])
+                        Eval += mult * (32 + (BISHOP if piece.color else R_BISHOP)[row][col])
                     case chess.KNIGHT:
                         Eval += mult * (30 + (KNIGHT if piece.color else R_KNIGHT)[row][col])
                     case chess.PAWN:
@@ -151,13 +142,13 @@ def quiescence(board, alpha, beta, depth):
     for move in sort_moves(board, list(board.generate_legal_captures())):
         positions += 1
         board.push(move)
-        score = -quiescence(board, -beta, -alpha, depth-1)
+        score = -quiescence(board, -beta, -alpha, depth - 1)
         board.pop()
 
         if score >= beta:
             return beta
 
-        if alpha > score:
+        if score > alpha:
             alpha = score
 
     return alpha
@@ -193,7 +184,7 @@ def root(board, depth):
 
         alpha = -1 * math.inf
         beta = math.inf
-        bestMoveFound = None
+        bestMoveFound = chess.Move.null()
         positions = 0
 
         for move in list(board.legal_moves):
@@ -223,140 +214,3 @@ def root(board, depth):
         print(f"nodes per second: {positions / time_taken}")
 
         return bestMoveFound
-
-
-def get_img(board, selected_square, player):  # Get board pygame image to render
-    if selected_square is not None:
-        svg = chess.svg.board(board=board, flipped=True if player == chess.BLACK else False,
-                              fill=dict.fromkeys(get_move_squares(board, selected_square), "#cc0000cc"))
-    else:
-        svg = chess.svg.board(board=board, flipped=True if player == chess.BLACK else False)
-
-    png_io = io.BytesIO()
-    cairosvg.svg2png(bytestring=bytes(svg, "utf8"), write_to=png_io)
-    png_io.seek(0)
-
-    surface = pygame.image.load(png_io, "png")
-    return surface
-
-
-def get_square(x, y, flipped):  # Get square number (0 - 63) from mouse coordinates
-    file = (x - 30) // (740 // 8)
-    rank = 8 - (y - 30) // (740 // 8) - 1
-    if flipped:
-        file = 7 - file
-        rank = 7 - rank
-    return file + rank * 8
-
-
-def get_move_squares(board, square):  # Get legal moves for each piece on the board
-    moves = []
-    for move in list(board.legal_moves):
-        move = str(move)
-        start, dest = move[:2], move[2:4]
-        if chess.square_name(square) == start:
-            moves.append(chess.parse_square(dest))
-    return moves
-
-
-def player_make_move(board, player, move_list):
-    if len(move_list) == 2:  # Player clicked twice
-        try:
-            promotion_squares = [i for i in range(8)] if player == chess.BLACK else [i for i in
-                                                                                     range(56, 64)]
-            if move_list[1] in promotion_squares and board.piece_at(
-                    move_list[0]).piece_type == chess.PAWN:  # Promotion
-                move = chess.Move(from_square=move_list[0], to_square=move_list[1], promotion=chess.QUEEN)
-            else:  # Non-Promotion
-                move = chess.Move(from_square=move_list[0], to_square=move_list[1])
-            if move in board.legal_moves:  # Move is legal
-                print(san := board.san(move))
-                board.push_san(san)
-            else:  # Illegal move
-                raise chess.IllegalMoveError
-        except chess.IllegalMoveError:
-            pass
-        except AttributeError:
-            pass
-        finally:
-            move_list = []
-    return board, player, move_list
-
-
-def computer_make_move(board):
-    move = root(board, 4)
-    if move is None:
-        move = list(board.legal_moves)[-1]
-    print(board.san(move))
-    board.push(move)
-    return board
-
-
-def main():  # Main Loop
-    board = chess.Board()
-
-    pygame.init()
-    pygame.display.set_caption("Chess")
-    display = pygame.display.set_mode((800, 800))
-    clock = pygame.time.Clock()
-    move_list = []
-    player = chess.BLACK
-    mode = Modes.COMPUTER_ONLY
-    gameOver = False
-
-    while True:
-        clock.tick(60)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    return
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                x, y = pygame.mouse.get_pos()
-                if pygame.mouse.get_pressed(3)[0]:
-                    if 30 <= x <= 800 - 30 and 30 <= y <= 800 - 30:
-                        move_list.append(get_square(x, y, player == chess.BLACK))
-
-        if not gameOver:  # If game is still ongoing
-            if mode == Modes.PLAYER_ONLY:
-                board, player, move_list = player_make_move(board, player, move_list)
-            elif mode == Modes.PLAYER_VS_COMPUTER:
-                if board.turn == player:
-                    board, player, move_list = player_make_move(board, player, move_list)
-                else:  # Computer's turn
-                    board = computer_make_move(board)
-            elif mode == Modes.COMPUTER_ONLY:  # Computer vs computer
-                board = computer_make_move(board)
-
-        # Render board
-        img = pygame.transform.scale(get_img(board, None if not move_list else move_list[0], player), (800, 800))
-        display.blit(img, (0, 0))
-        pygame.display.flip()
-
-        if not gameOver:  # game is not over
-            if board.is_checkmate():  # Checkmate
-                print(title := f"{'White' if not board.turn else 'Black'} won the game by checkmate!")
-                pygame.display.set_caption(f"Chess: {title}")
-                gameOver = True
-
-            if board.is_stalemate():  # Stalemate
-                print(title := "Draw by stalemate")
-                pygame.display.set_caption(f"Chess: {title}")
-                gameOver = True
-
-            if board.can_claim_draw():
-                print(title := "Draw by threefold repetition")
-                pygame.display.set_caption(f"Chess: {title}")
-                gameOver = True
-
-            if board.is_insufficient_material():
-                print(title := "Draw by insufficient material")
-                pygame.display.set_caption(f"Chess: {title}")
-                gameOver = True
-
-
-if __name__ in "__main__":  # Run the game
-    main()
